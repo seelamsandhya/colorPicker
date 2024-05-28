7a8d673ac152d749b5d94bb0a955c316d66538e2
sandhya <seelamsandhya201999@gmail.com>
2024-05-28 16:01:22 +0530

    final commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function changeGrey(){
     document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
+    document.getElementById("Hexcode").style.color = "#e0e0e0";
+    document.getElementById("Hexcode").textContent = "#e0e0e0";
+}
+function changeGreen(){
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97";
+    document.getElementById("Hexcode").style.color = "#e0e0e0";
+    document.getElementById("Hexcode").textContent = "#e0e0e0";
+
 }
