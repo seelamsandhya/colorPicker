288922fa090a56ae1bfdcd198340665471fc8458
sandhya <seelamsandhya201999@gmail.com>
2024-05-31 11:10:44 +0530

    final commit 3

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-function changeGrey(){
+ function changeGrey(){
     document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
     document.getElementById("Hexcode").style.color = "#e0e0e0";
     document.getElementById("Hexcode").textContent = "#e0e0e0";
 }
-function changeGreen(){
+ function changeGreen(){
     document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97";
+    document.getElementById("Hexcode").style.color = "#e0e0e0";
+    document.getElementById("Hexcode").textContent = "#e0e0e0";
+
+}
+
+function changeBlue(){
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf90";
+    document.getElementById("Hexcode").style.color = "#e0e0e0";
+    document.getElementById("Hexcode").textContent = "#e0e0e0";
+
+    
+}
+function changePurple(){
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf22";
     document.getElementById("Hexcode").style.color = "#e0e0e0";
     document.getElementById("Hexcode").textContent = "#e0e0e0";
 
